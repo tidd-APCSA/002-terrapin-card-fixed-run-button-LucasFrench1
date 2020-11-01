56c4520457b7349c6040b9dd8d92d0ec9b5cd96f
LucasFrench1 <72262822+LucasFrench1@users.noreply.github.com>
2020-11-01 21:47:12 +0000

    implemented and used the terrapin class

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,38 @@
 public class TerrapinCard {
+  private double balance;
+  public TerrapinCard(double bal){
+    balance = bal;
+  }
+  public TerrapinCard(){
+    this(0);
+  }
 
+  public String toString(){
+    return "This card has "+balance+" dollars";
+  }
 
+  public TerrapinCard payEconomical(){
+    if((balance - 2.5) >= 0){
+      balance -= 2.5;
+    }
+    return this;
+  }
+
+  public TerrapinCard payGourmet(){
+    if((balance - 4) >= 0){
+      balance -= 4;
+    }
+    return this;
+  }
+
+  public TerrapinCard loadMoney(double moneyToAdd){
+    if(moneyToAdd < 0){}
+    else if(!((balance+moneyToAdd) > 200)){
+      balance += moneyToAdd;
+    }
+    else{
+      balance = 200;
+    }
+    return this;
+  }
 }
